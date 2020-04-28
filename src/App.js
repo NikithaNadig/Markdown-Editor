56a04fe32c420cf3009d91330332b22d41b17f73
Nikitha Nadig <nadig.nikitha@gmail.com>
2020-04-29 03:19:18 +0530

    Change theme using a useContext() hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Markdown from './components/markdown';
 import Header from './components/header'
+import ThemeContext, {themes} from './components/themes-context'
 
 function App() {
+  const [theme,setTheme] = useState(themes.dark);
+  const changeTheme = () => {
+  theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
+  }
   return (
     <div className="App">
-    <Header />Hi
+    <ThemeContext.Provider value={theme}>
+      <button onClick={changeTheme}>TOGGLE</button>
+    <Header />
+    </ThemeContext.Provider>
     <Markdown />  
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import  './header.css';
+import ThemeContext from './themes-context';
 
 const Header = () => {
+    const theme= useContext(ThemeContext)
     return(
-        <div className="header">MARKDOWN EDITOR</div>
+        <div style={theme}>
+            <p>{JSON.stringify(theme)}</p>
+        </div>
     )
 }
 
